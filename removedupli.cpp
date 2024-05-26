55df4caa14147070a079bf6a58fc366e936861fe
divi <107640606+Divi1107@users.noreply.github.com>
2024-05-26 18:49:53 +0530

    Create removedupli.cpp

---
+++ removedupli.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        int n = nums.size();
+        for(int j=1;j<n;j++)
+        {
+            if(nums[j]!= nums[i])
+            {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+       // int c = nums.size();
+        
+        return i+1;
+    }
+};
