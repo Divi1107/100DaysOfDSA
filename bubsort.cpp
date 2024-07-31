2d62d46745341fea8c6c8778e0cb96c4003f20d9
divi <107640606+Divi1107@users.noreply.github.com>
2024-07-31 22:06:14 +0530

    Create bubsort.cpp

---
+++ bubsort.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n) {
+       for(int i=0;i<n;i++)
+       {   
+           for(int j = i+1;j < n;j++)
+           {
+               if(arr[i]>arr[j])
+               {
+                   int temp = arr[i];
+                   arr[i] = arr[j];
+                   arr[j] = temp;
+                   //swapp =1;
+               }
+           }
+       }
+    }
+};
